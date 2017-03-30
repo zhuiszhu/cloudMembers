325f6ec80926528049eb9d8a5f3f5080e00288df
zhuiszhu <1799150010@qq.com>
2017-03-31 00:16:34 +0800

    登录页面基础样式编写(style.css) 生成资源目录resources 生成功能目录functions 导入ba.less资源文件 导入ba.css文件 修改首页基础路由,规避报错 修改loginPage模板,添加输入行class样式 修改首页模板index.ejs引入ba.css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send("this is home page");
 });
 
 module.exports = router;
